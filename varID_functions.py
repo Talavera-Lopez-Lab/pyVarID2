70f152285dc97d40759455f2c16ebd2d979100c9
egerc <eger.christian@gmx.de>
2024-05-16 17:51:14 +0200

    getExpData works now

--- varID_functions.py
+++ varID_functions.py
@@ -1,7 +1,7 @@
-from RaceID3_StemID2_class import SCseq
+from RaceID import SCseq
 
 def getExpData(SCseq_object, genes=None):
+    expdata = SCseq_object.expdata
     if genes == None:
-        # the genes attribute must be assigned in the filterdata method however i cant find where
         genes = SCseq_object.genes
-    return None
+    return expdata[expdata.index.isin(genes)]
